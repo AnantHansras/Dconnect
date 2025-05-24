c4c70b43820ab14692c926f9f0179df9fc267ecc
AnantHansras <ananthansras.16@gmail.com>
2025-05-24 16:28:13 +0530

    trailing / removed

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173/", "https://dconnect-eight.vercel.app/"],
+    origin: ["http://localhost:5173", "https://dconnect-eight.vercel.app"],
     credentials: true,
   })
 );
